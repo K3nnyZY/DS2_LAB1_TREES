235e5163aeb7a163038d190a5381b77322d10a98
Kenny Zhu Ye <kennyzhu@MacBook-Air-de-Kenny.local>
2023-03-02 11:08:24 -0500

    c

--- punto_1/Node.py
+++ punto_1/Node.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, user_id, user_name):
-        self.user_id = user_id
+    def __init__(self, user_name, user_id) -> None:
         self.user_name = user_name
-        self.height = 1
+        self.user_id = user_id
         self.left = None
         self.right = None
+        self.height = 1
---
+++ punto_1/app.py
@@ -0,0 +1,13 @@
+from AVL import AVL_Tree
+
+tree = AVL_Tree()
+
+csv_files = ["./csv/User_track_data.csv", "./csv/User_track_data_2.csv", "./csv/User_track_data_3.csv"]
+tree.build_tree(csv_files)
+
+print("\nlevel order traversal")
+tree.printLevelOrder(tree.root)
+
+print("\n borrar")
+tree.delete_node(tree.root, 2359)
+tree.printLevelOrder(tree.root)
